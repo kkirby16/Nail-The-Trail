bb84b550c1863d6c6869e7655756a278df1e2789
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 00:20:04 -0800

    add custom route for /signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   root "application#home"
   get "/login", to: "sessions#new", as: "login"
+  get "/signup", to: "users#new", as: "signup"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

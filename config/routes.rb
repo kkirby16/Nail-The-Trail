cbd83ccae6de89186a3fc1a0c1cb0ea5b05acf37
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 01:20:56 -0800

    add custom post /login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   root "application#home"
   get "/login", to: "sessions#new", as: "login"
+  post "/login", to: "sessions#create", as: "login-post"
   get "/signup", to: "users#new", as: "signup"
   delete "/logout", to: "sessions#destroy", as: "logout"
 

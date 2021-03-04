30ed5f742be9e4f2c723d002c48f6f474797531e
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 15:17:49 -0800

    add model presence validations for name, username, city and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reviews
   has_many :hikes, through: :reviews
+  validates :name, presence: true
+  validates :username, presence: true
+  validates :city, presence: true
+  validates :password, presence: true
 end

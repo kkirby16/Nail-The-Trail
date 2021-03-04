fa343aaf8dacbb417335d9b37b916f6a29ac38d5
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 00:12:39 -0800

    start coding create action of users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+        session[:user_id] = @user.id
+    end
   end
 
   def show

555669c9a4c08bad8bc3c77e032bf77d30e97855
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 00:49:23 -0800

    add custom /logout route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root "application#home"
   get "/login", to: "sessions#new", as: "login"
   get "/signup", to: "users#new", as: "signup"
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

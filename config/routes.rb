61e98d243a69e8b9f3de6d722ee06ed7d036bf10
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-03 17:30:28 -0800

    make the root route and a custom login route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :hikes
   resources :reviews
   resources :users
+  root "application#home"
+  get "/login", to: "sessions#new", as: "login"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

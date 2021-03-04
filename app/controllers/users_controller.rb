04d1a1565cbb461b2977ebed9b463b3ed0d2ef55
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 00:42:26 -0800

    code out more of the create action in the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save 
-        session[:user_id] = @user.id
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to signup_path
     end
   end
 

c0e7d06e52e90c8130608e1017983e5128303149
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-03 17:26:38 -0800

    add some actions to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password, :city, :bio, :years_hiking, :admin)
+  end
 end

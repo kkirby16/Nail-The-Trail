2005e9454cc41cfc95886aa48133528251f128cb
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 16:47:31 -0800

    change successful creation of new user to redirect to welcome page instead of user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to welcome_path
     else
       redirect_to signup_path
     end
+  end
+
+  def welcome
   end
 
   def show

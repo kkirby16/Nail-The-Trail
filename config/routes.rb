63cb71008a7e00d3e100f6159a9428ec7422ca69
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 16:48:09 -0800

    add custom route for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create", as: "login-post"
   get "/signup", to: "users#new", as: "signup"
   delete "/logout", to: "sessions#destroy", as: "logout"
+  get "/welcome", to: "users#welcome", as: "welcome"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

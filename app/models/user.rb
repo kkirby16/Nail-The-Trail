3359ff35a8f4f3a157c6b969d40d759c779d5b50
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-04 22:59:54 -0800

    add uniqueness validator for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :city, presence: true
   validates :password, presence: true
+  validates :username, uniqueness: true
 end

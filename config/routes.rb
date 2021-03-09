5a7feb9f11730839d9ac69407cd125a21ba207b4
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-08 20:50:27 -0800

    change custom /welcome route to have /home as a url instead

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create", as: "login-post"
   get "/signup", to: "users#new", as: "signup"
   delete "/logout", to: "sessions#destroy", as: "logout"
-  get "/welcome", to: "users#welcome", as: "welcome"
+  get "/home", to: "users#welcome", as: "welcome"
   get "/profile/:id", to: "users#show", as: "profile"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

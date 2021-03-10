db04808d7e8c919f72378114a28aeaeba9ae18ea
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-09 20:16:13 -0800

    add a few notes for myself.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy", as: "logout"
   get "/home", to: "users#welcome", as: "welcome"
   get "/profile/:id", to: "users#show", as: "profile"
+  #need something like get "/users/:id/reviews" which would be a nested resource index.
+  #need something like get "/users/1/reviews" which would be a url for my nested resource index.
+  #get "/hikes_by_city", to: "hikes#hikes_by_city", as "hikes_by_city" #this is the route that takes us to the page showing hikes by city
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

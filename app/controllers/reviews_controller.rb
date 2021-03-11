393f93133925786851c08acb19c78b5eb3c3c44e
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-10 20:57:01 -0800

    add index action, and method definitions for new and create actions.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def index
+    if params[:hike_id]
+      @reviews = Hike.find(params[:hike_id]).reviews
+    else
+      @reviews = Review.all
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  #create strong params method too.
 end

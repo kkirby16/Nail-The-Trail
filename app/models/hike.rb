818daacfc2edd60e1318108b8b4a9151909c82c0
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-10 21:18:14 -0800

    create a self.search class method that takes in a city name that a user selected from a dropdown box, finds all hikes that are located in that city, and returns those hikes located in that city.

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,4 +1,14 @@
 class Hike < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  def self.search(search)
+    if search
+      hikes = Hike.all
+      hikes = hikes.where(location: search)
+      return hikes
+    else
+      Hike.all
+    end
+  end
 end

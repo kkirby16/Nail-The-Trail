b2ed722dba25f15af994b7b1a9166fbe0cf16e18
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-12 22:46:31 -0800

    Revert "change new action by using .build to set @review"
    This reverts commit 7ae86ae984fcfdfa0629a0b39f59c48710ff547f.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class ReviewsController < ApplicationController
   def index
     if params[:hike_id]
@@ -11,22 +9,10 @@ class ReviewsController < ApplicationController
 
   def new
     @hike = Hike.find(params[:hike_id])
-    @review = @hike.reviews.build(review_params)
+    @review = Review.new
   end
 
   def create
-    @review = Review.new(review_params)
-    if @review.save
-      redirect_to hike_reviews_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def review_params
-    params.permit(:user_id, :hike_id, :title, :description, :date, :star_rating)
   end
 
   #create strong params method too.

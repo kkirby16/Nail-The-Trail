85ecfe4d8aff7e5d1df54b1f9af4331e799ec1ca
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-16 15:33:24 -0700

    change @review variable in new action, change @hike variable in create action, change redirect path if review saves to include @hike and have this redirect display a notice, and add params.require(:review) to review_params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,14 +11,14 @@ class ReviewsController < ApplicationController
 
   def new
     @hike = Hike.find(params[:hike_id])
-    @review = @hike.reviews.build(review_params)
+    @review = @hike.reviews.build
   end
 
   def create
-    @hike = Hike.find_by(id: params[:hike_id])
+    @hike = Hike.find_by(id: params[:review][:hike_id])
     @review = Review.new(review_params)
     if @review.save
-      redirect_to hike_reviews_path
+      redirect_to hike_reviews_path(@hike), notice: "Thanks for creating a review! It's now listed below!"
     else
       render :new
     end
@@ -27,7 +27,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.permit(:user_id, :hike_id, :title, :description, :date, :star_rating)
+    params.require(:review).permit(:user_id, :hike_id, :title, :description, :date, :star_rating)
   end
 
   #create strong params method too.

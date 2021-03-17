0957f087a766dc9b647beab8ca4c15b5cefc002f
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-16 21:16:27 -0700

    add edit action so that a user can see a form for being able to edit some of their basic user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,10 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
   private
 
   def user_params

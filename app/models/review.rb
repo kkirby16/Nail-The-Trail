2ee9c813c76a0a896e5b068da78b73bd750d78e9
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-16 17:48:29 -0700

    add presence model validations for title, description, date, and star rating and add numericality validation for star rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :hike
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
+  validates :star_rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end

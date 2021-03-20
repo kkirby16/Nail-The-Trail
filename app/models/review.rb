b9302bfe3c522aa2c5a4d0d5d77564fbbd12e0c5
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-19 22:48:00 -0700

    create update_hikes_average method that updates the hikes avg_star_rating attribute with the average rating from its reviews and added after_save and after_destroy callbacks that call update_hikes_average so a hikes average rating is constantly updating.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,18 @@
+require "pry"
+
 class Review < ApplicationRecord
+  after_save :update_hikes_average
+  after_destroy :update_hikes_average
+
   belongs_to :user
   belongs_to :hike
   validates :title, presence: true
   validates :description, presence: true
   validates :date, presence: true
   validates :star_rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  def update_hikes_average
+    new_avg = (self.hike.reviews.sum { |review| review.star_rating.to_f } / self.hike.reviews.count.to_f)
+    self.hike.update(avg_star_rating: new_avg.to_f)
+  end
 end

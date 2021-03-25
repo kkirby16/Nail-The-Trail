429ca66e67f1796334a1b3c6ed5a332e1411a5b3
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-25 10:28:35 -0700

    add a require_login method to make sure a user is logged in and add before_action filter that calls that require_login method

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,11 +1,17 @@
 require "pry"
 
 class HikesController < ApplicationController
+  before_action :require_login
+
   def index
     if params[:hike][:":location"] != "None"
       @hikes = Hike.search(params[:hike][:":location"])
     else
       @hikes = Hike.all
     end
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
   end
 end

f0a227278f59e200fdc9f8f14ec168eae34995ee
kkirby16 <kkirby16@my.whitworth.edu>
2021-03-31 19:52:57 -0700

    consolidate all presence validations onto their own single line of code

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,10 +6,8 @@ class Review < ApplicationRecord
 
   belongs_to :user
   belongs_to :hike
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :date, presence: true
-  validates :star_rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :title, :description, :date, :star_rating, presence: true
+  validates :star_rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
   def update_hikes_average
     new_avg = (self.hike.reviews.sum { |review| review.star_rating.to_f } / self.hike.reviews.count.to_f)

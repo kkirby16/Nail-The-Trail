e9e82d79fde27b6ad5be7ae5359175b7b596aa40
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-23 00:20:36 -0800

    remigrated db for PostgreSQL switch

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_03_075921) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hikes", force: :cascade do |t|
     t.string "name"
     t.string "location"

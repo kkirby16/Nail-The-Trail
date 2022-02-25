12f342dc8fc05db2b60ef4fff528966ba47c319c
kkirby16 <kkirby16@my.whitworth.edu>
2022-02-24 21:20:56 -0800

    change t.string columns to be t.text

--- db/migrate/20210303075921_create_hikes.rb
+++ db/migrate/20210303075921_create_hikes.rb
@@ -1,8 +1,8 @@
 class CreateHikes < ActiveRecord::Migration[6.0]
   def change
     create_table :hikes do |t|
-      t.string :name
-      t.string :location
+      t.text :name
+      t.text :location
       t.text :description
       t.float :avg_star_rating, default: 0, precision: 3, scale: 2
       t.text :difficulty

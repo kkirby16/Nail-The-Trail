0935c43d505aba6163d72a92cad991427b039ed3
kkirby16 <kkirby16@my.whitworth.edu>
2023-08-26 01:24:44 -0700

    run rails db:migrate to create the schema again

---
+++ db/schema.rb
@@ -0,0 +1,50 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_03_075921) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "hikes", force: :cascade do |t|
+    t.text "name"
+    t.text "location"
+    t.text "description"
+    t.float "avg_star_rating", default: 0.0
+    t.text "difficulty"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "hike_id"
+    t.text "title"
+    t.text "description"
+    t.date "date"
+    t.integer "star_rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.text "username"
+    t.text "email"
+    t.text "city", default: "unspecified"
+    t.text "bio", default: "unspecified"
+    t.text "years_hiking", default: "unspecified"
+    t.text "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+end
